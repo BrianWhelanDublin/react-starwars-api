526f6e14a986ae3d6014bf000e004f74630703a7
Brian Whelan <brian@friday.ie>
2022-01-01 21:12:10 +0000

    add firebase to use get http request

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 
 import MoviesList from './components/MoviesList';
+import AddMovie from './components/AddMovie';
 import './App.css';
 
 function App() {
@@ -31,26 +32,41 @@ function App() {
     setLoading(true)
     setError(null)
     try {
-      const res = await fetch("https://swapi.dev/api/films")
+      const res = await fetch(process.env.REACT_APP_FIREBASE_URL)
       if (!res.ok) {
         throw new Error("Something went wrong")
       }
 
       const data = await res.json();
+      console.log(data)
 
-      const moviesList = data.results.map(movie => {
-        return {
-          id: movie.episode_id,
-          title: movie.title,
-          openingText: movie.opening_crawl,
-          releaseDate: movie.release_date
-        }
-      })
-      setMovies(moviesList)
+      const loadedMovies = []
+
+      for (const key in data) {
+        loadedMovies.push({
+          id: key,
+          title: data[key].title,
+          openingText: data[key].openingText,
+          releaseDate: data[key].releaseDate
+        })
+      }
+      setMovies(loadedMovies)
     } catch (error) {
       setError(error.message)
     }
     setLoading(false)
+  }
+
+  const addMovieHandler = async (movie) => {
+    const res = await fetch(process.env.REACT_APP_FIREBASE_URL, {
+      method: "POST",
+      body: JSON.stringify(movie),
+      header: {
+        "Content-type": "application/json"
+      }
+    })
+    const data = await res.json()
+    console.log(data)
   }
 
   let content = <p>Click for movies</p>
@@ -69,11 +85,12 @@ function App() {
   return (
     <React.Fragment>
       <section>
-        <button onClick={fetchMoviesAsync}>Fetch Movies</button>
+        <AddMovie onAddMovie={addMovieHandler} />
       </section>
       <section>
-        {content}
+        <button onClick={fetchMoviesAsync}>Fetch Movies</button>
       </section>
+      <section>{content}</section>
     </React.Fragment>
   );
 }
